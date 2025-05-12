bce9fda49db0f9cfa2eaec53a6eecd869c1a52d1
fharleston2 <168579170+fharleston2@users.noreply.github.com>
2025-05-12 22:33:19 +1000

    added get requests

--- controllers/StatesController.js
+++ controllers/StatesController.js
@@ -13,9 +13,9 @@ const getAllStates = (req, res) => {
 const getState = (req, res) => {
     const allStateCodes = data.states.map(state => state.code.toUpperCase());
    
-        const stateCode = req.params.state.toUpperCase();
+    const stateCode = req.params.state.toUpperCase();
         
-        const state = data.states.find(state => state.code.toUpperCase() === stateCode);
+    const state = data.states.find(state => state.code.toUpperCase() === stateCode);
         if (!state) {
             return res.status(404).json({ message: 'State not found' });
         }
@@ -24,20 +24,20 @@ const getState = (req, res) => {
         
     }
 
-    const getContigStates = (req, res) => {
+const getContigStates = (req, res) => {
        
-            const contigStates = data.states.filter(state => !['HI', 'AK'].includes(state.code));
-            res.json(contigStates);
+    const contigStates = data.states.filter(state => !['HI', 'AK'].includes(state.code));
+    res.json(contigStates);
     }
 
-    const getNonContigStates = (req, res) => {
+const getNonContigStates = (req, res) => {
         const nonContigStates = data.states.filter(state => ['HI', 'AK'].includes(state.code));
         res.json(nonContigStates);
     }   
 
-    const getStateCapital = (req, res) => {
-        const stateCode = req.params.state.toUpperCase();
-        const state = data.states.find(state => state.code.toUpperCase() === stateCode);
+ const getStateCapital = (req, res) => {
+    const stateCode = req.params.state.toUpperCase();
+    const state = data.states.find(state => state.code.toUpperCase() === stateCode);
         if (!state) {
             return res.status(404).json({ message: 'Invalid state abbreviation parameter' });
         }
@@ -46,11 +46,47 @@ const getState = (req, res) => {
             capital: state.capital_city });
     }
 
+const getStateNickname = (req, res) => {
+    const stateCode = req.params.state.toUpperCase();
+    const state = data.states.find(state => state.code.toUpperCase() === stateCode);
+    if (!state) {
+        return res.status(404).json({ message: 'Invalid state abbreviation parameter' });
+    }
+    res.json({ 
+        state: state.state,
+        nickname: state.nickname });
+}
+
+const getStatePopulation = (req, res) => {
+    const stateCode = req.params.state.toUpperCase();
+    const state = data.states.find(state => state.code.toUpperCase() === stateCode);
+    if (!state) {
+        return res.status(404).json({ message: 'Invalid state abbreviation parameter' });
+    }
+    const formatedPop = state.population.toLocaleString();
+    res.json({ 
+        state: state.state,
+        population: formatedPop });
+    }
+const getStateAdmission = (req, res) => {
+    const stateCode = req.params.state.toUpperCase();
+    const state = data.states.find(state => state.code.toUpperCase() === stateCode);
+    if (!state) {
+        return res.status(404).json({ message: 'Invalid state abbreviation parameter' });
+    }
+    res.json({ 
+        state: state.state,
+        admission: state.admission_date });
+    }
+
 
     module.exports ={
         getAllStates,
         getState,
         getContigStates,
         getNonContigStates,
-        getStateCapital
+        getStateCapital,
+        getStateNickname,
+        getStatePopulation,
+        getStateAdmission
     }
--- routes/api/states.js
+++ routes/api/states.js
@@ -23,8 +23,19 @@ router.route('/')
    .get(statesController.getContigStates); */
 
 
-   router.route('/:state/capital')
+router.route('/:state/capital')
     .get(statesController.getStateCapital);
+
+router.route('/:state/nickname')
+    .get(statesController.getStateNickname);
+
+router.route('/:state/population')
+    .get(statesController.getStatePopulation);
+
+router.route('/:state/admission')
+    .get(statesController.getStateAdmission); 
+
+
 
 router.route('/:state')
    .get(statesController.getState);
